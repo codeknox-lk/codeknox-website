3b472e166836845aff281f5dd4ef4aad934a3bac
Ruwantha Kavishan Ekanayake <ruwanthakavishanekanayake@Mac.lan>
2025-09-16 08:43:16 +0530

    Test auto-deployment - Tue Sep 16 08:43:16 +0530 2025

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// Auto-deployment test - $(date)
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { BlogProvider } from "./contexts/BlogContext";
 import { ProjectProvider } from "./contexts/ProjectContext";
