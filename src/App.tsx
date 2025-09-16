999f35ed92c1c35887a6d61e4d82e836adfbdb73
Ruwantha Kavishan Ekanayake <ruwanthakavishanekanayake@Mac.lan>
2025-09-16 08:44:05 +0530

    Clean up test comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// Auto-deployment test - $(date)
 import { ThemeProvider } from "./contexts/ThemeContext";
 import { BlogProvider } from "./contexts/BlogContext";
 import { ProjectProvider } from "./contexts/ProjectContext";
